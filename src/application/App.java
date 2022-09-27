406638f2542752ed396469f4068194324359f1fe
Danillo Moraes <moraesdanillo10@gmail.com>
2022-09-27 20:20:58 -0300

    Exibição dos emails ordenados e soma dos salários

--- src/application/App.java
+++ src/application/App.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 import entities.Empregado;
 
@@ -29,6 +30,24 @@ public class App {
             for (Empregado empregado : empregados) {
                 System.out.println(empregado);
             }
+
+            //E-mails em ordem alfabética dos funcionários que recebem mais que o usuário digitar
+            System.out.println("Entre com o salário (serão listados os funcionários que recebem mais que o digitado):");
+            double salario = sc.nextDouble();
+
+            List<String> emails = empregados.stream()
+                                    .filter(p -> p.getSalario() > salario)
+                                    .map(p -> p.getEmail()).sorted()
+                                    .collect(Collectors.toList());
+            
+            emails.forEach(System.out::println);
+
+            double soma = empregados.stream()
+                                    .filter(p -> p.getNome().startsWith("M"))
+                                    .map(p -> p.getSalario())
+                                    .reduce(0.0, (x,y) -> x + y);
+
+            System.out.println("Soma dos salários de quem começa com M: " + soma);
 
         } catch (Exception e) {
             System.out.println("Erro: " + e.getMessage());
